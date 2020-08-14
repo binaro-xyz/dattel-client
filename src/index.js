cba091bf709461d56e7f97982bb26539f76c8d7d
Benjamin Altpeter <benjamin@oldpeter.de>
2020-08-14 14:55:04 +0200

    Initial commit: Support for createSite

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const bent = require('bent');
+
+let config;
+try {
+    config = require('../config.json');
+} catch (_) {
+    console.error(
+        'Invalid or missing configuration. Please copy `config-sample.json` to `config.json` and change the values as needed.'
+    );
+    process.exit(1);
+}
+
+const request = (method) =>
+    bent(method, 'json', [200, 201], config.server_url, {
+        Authorization: `Bearer ${config.auth_token}`,
+    });
+const GET = request('GET');
+const POST = request('POST');
+const PUT = request('PUT');
+const DELETE = request('DELETE');
+
+const catcher = async (e) => console.error((await e.json()) || e.text());
+module.exports = {
+    raw: request,
+    createSite: async (site_id, domain) => PUT('/site', { site_id, domain }).catch(catcher),
+};
